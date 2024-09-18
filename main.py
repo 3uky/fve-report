4ccfc5b4e44b59c80bd7d7477aeb864ae360953e
3uky <hynek.bucek@gmail.com>
2024-09-18 22:13:55 +0200

    print OTE method with currency conversion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from inverter.goodwe import *
-
+from datetime import datetime
+import pandas as pd
+from currency_converter import CurrencyConverter
 
 def print_invertor_report(inverter):
     print(f"PV: \t{inverter.get_pv()} W")
@@ -7,10 +9,18 @@ def print_invertor_report(inverter):
     print(f"Grid:\t{inverter.get_power_flow()} W")
     print(f"Dum:\t{inverter.get_home_consumption()} W")
 
+def print_ote_actual_price():
+    hour = datetime.now().hour
+    ote = pd.read_csv('data/output.csv', index_col=0)
+    price_EUR = ote.loc[hour, 'price']
+    price_CZK = CurrencyConverter().convert(price_EUR, 'EUR', 'CZK')
+    print(f"OTE:\t{price_EUR:.2f} EUR/MW  {price_CZK:.0f} Kc/MW")
+
 
 def main():
     print_invertor_report(Inverter())
+    print_ote_actual_price()
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
