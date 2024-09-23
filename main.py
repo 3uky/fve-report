c89b89437fa110a92e0095a42ac9a526a54ac478
3uky <hynek.bucek@gmail.com>
2024-09-23 19:06:50 +0200

    refactoring - report classes

--- main.py
+++ main.py
@@ -3,23 +3,38 @@ from datetime import datetime
 import pandas as pd
 from currency_converter import CurrencyConverter
 
-def print_invertor_report(inverter):
-    print(f"PV: \t{inverter.get_pv()} W")
-    print(f"Bat:\t{inverter.get_batteries_flow()} W  {inverter.get_batteries_soc()} %")
-    print(f"Grid:\t{inverter.get_power_flow()} W")
-    print(f"Dum:\t{inverter.get_home_consumption()} W")
 
-def print_ote_actual_price():
-    hour = datetime.now().hour
-    ote = pd.read_csv('data/output.csv', index_col=0)
-    price_EUR = ote.loc[hour, 'price']
-    price_CZK = CurrencyConverter().convert(price_EUR, 'EUR', 'CZK')
-    print(f"OTE:\t{price_EUR:.2f} EUR/MW  {price_CZK:.0f} Kc/MW")
+class OTE:
+    def __init__(self):
+        self.ote = pd.read_csv('data/output.csv', index_col=0)
+
+    def __str__(self):
+        return f"OTE:\t{self.get_actual_price_czk():.0f} Kc/MW  {self.get_actual_price_eur():.2f} EUR/MW"
+        # return f"OTE:\t{self.get_actual_price_czk():.0f} Kc/MW"
+
+    def get_actual_price_eur(self):
+        return self.ote.loc[datetime.now().hour, 'price']
+
+    def get_actual_price_czk(self):
+        return CurrencyConverter().convert(self.get_actual_price_eur(), 'EUR', 'CZK')
+
+
+class Inverter:
+    def __init__(self):
+        self.inverter = GoodweInverter()
+
+    def __str__(self):
+        return f"PV: \t{self.inverter.get_pv()} W\n" \
+               f"Bat:\t{self.inverter.get_batteries_flow()} W  {self.inverter.get_batteries_soc()} %\n" \
+               f"Grid:\t{self.inverter.get_power_flow()} W\n" \
+               f"Dum:\t{self.inverter.get_home_consumption()} W\n"
 
 
 def main():
-    print_invertor_report(Inverter())
-    print_ote_actual_price()
+    inverter = Inverter()
+    ote = OTE()
+
+    print(str(inverter) + str(ote))
 
 
 if __name__ == "__main__":
