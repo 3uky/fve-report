47ab35ef58bcc3908beed98dc53899ad693cc009
3uky <hynek.bucek@gmail.com>
2024-11-03 13:08:36 +0100

    main loop

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pandas as pd
 from currency_converter import CurrencyConverter
 
+import time
 
 class OTE:
     def __init__(self):
@@ -21,20 +22,35 @@ class OTE:
 
 class Inverter:
     def __init__(self):
-        self.inverter = GoodweInverter()
+        self.refresh()
+
+    def refresh(self):
+        self.goodwe = GoodweInverter()
+        self.solar = self.goodwe.get_pv()
+        self.bat_W = self.goodwe.get_batteries_flow()
+        self.bat_per = self.goodwe.get_batteries_soc()
+        self.grid = self.goodwe.get_power_flow()
+        self.consumption = self.goodwe.get_home_consumption()
 
     def __str__(self):
-        return f"PV: \t{self.inverter.get_pv()} W\n" \
-               f"Bat:\t{self.inverter.get_batteries_flow()} W  {self.inverter.get_batteries_soc()} %\n" \
-               f"Grid:\t{self.inverter.get_power_flow()} W\n" \
-               f"Dum:\t{self.inverter.get_home_consumption()} W\n"
+        return f"PV: \t{self.solar} W\n" \
+               f"Bat:\t{self.bat_W} W  {self.bat_per} %\n" \
+               f"Grid:\t{self.grid} W\n" \
+               f"Dum:\t{self.consumption} W\n"
 
 
 def main():
-    inverter = Inverter()
-    ote = OTE()
+    try:
+        ote = OTE()
+        inverter = Inverter()
 
-    print(str(inverter) + str(ote))
+        while(True):
+            inverter.refresh()
+            print(str(inverter) + str(ote), flush=True)
+            time.sleep(10)
+
+    except KeyboardInterrupt:
+        exit()
 
 
 if __name__ == "__main__":
