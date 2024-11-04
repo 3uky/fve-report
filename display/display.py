c43bfa4227d1bf89e5d3986afc96b429393351e8
3uky <hynek.bucek@gmail.com>
2024-11-04 13:51:11 +0100

    refactoring - display code separation

---
+++ display/display.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import logging
+
+from display.lib.waveshare_epd import epd2in13_V2
+from PIL import Image,ImageDraw,ImageFont
+
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'display', 'pic')
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class Display:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def update(inverter, ote):
+        logging.info("epd2in13_V2 display init")
+        epd = epd2in13_V2.EPD()
+        epd.init(epd.FULL_UPDATE)
+        epd.Clear(0xFF)
+        font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
+
+        image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+        draw = ImageDraw.Draw(image)
+
+        draw.text((20, 0), f'PV: {inverter.solar} W', font=font24, fill=0)
+        draw.text((20, 24), f'Bat: {inverter.bat_W} W  {inverter.bat_per} %', font=font24, fill=0)
+        draw.text((20, 48), f'Grid: {inverter.grid} W', font=font24, fill=0)
+        draw.text((20, 72), f'Dum: {inverter.consumption} W', font=font24, fill=0)
+        draw.text((20, 96), f'OTE: {ote.get_actual_price_czk():.0f} Kc/MW  {ote.get_actual_price_eur():.2f} EUR/MW', font=font24, fill=0)
+        epd.display(epd.getbuffer(image))
+
+        logging.info("Goto Sleep...")
+        epd.sleep()
+
+    def __del__(self):
+        logging.info("display module exit")
+        epd2in13_V2.epdconfig.module_exit(cleanup=True)
--- main.py
+++ main.py
@@ -2,21 +2,12 @@ from inverter.goodwe import *
 from datetime import datetime
 import pandas as pd
 from currency_converter import CurrencyConverter
+from display.display import Display
 
 import logging
 import time
-import sys
-import os
 
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'fve-report', 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'fve-report', 'lib')
-if os.path.exists(libdir):
-    sys.path.append(libdir)
 
-logging.basicConfig(level=logging.DEBUG)
-
-from waveshare_epd import epd2in13_V2
-from PIL import Image,ImageDraw,ImageFont
 
 class OTE:
     def __init__(self):
@@ -51,46 +42,24 @@ class Inverter:
                f"Grid:\t{self.grid} W\n" \
                f"Dum:\t{self.consumption} W\n"
 
-def update_displayed_data(inverter, ote):
-    logging.info("epd2in13_V2 Demo")
-
-    epd = epd2in13_V2.EPD()
-    epd.init(epd.FULL_UPDATE)
-    epd.Clear(0xFF)
-
-    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
-
-    image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    draw = ImageDraw.Draw(image)
-
-    draw.text((20, 0), f'PV: {inverter.solar} W', font=font24, fill=0)
-    draw.text((20, 24), f'Bat: {inverter.bat_W} W  {inverter.bat_per} %', font=font24, fill=0)
-    draw.text((20, 48), f'Grid: {inverter.grid} W', font=font24, fill=0)
-    draw.text((20, 72), f'Dum: {inverter.consumption} W', font=font24, fill=0)
-    draw.text((20, 96), f'OTE: {ote.get_actual_price_czk():.0f} Kc/MW  {ote.get_actual_price_eur():.2f} EUR/MW', font=font24, fill=0)
-    epd.display(epd.getbuffer(image))
-
-    logging.info("Goto Sleep...")
-    epd.sleep()
 
 def main():
     try:
-        print(f'libdir: {libdir}')
         ote = OTE()
         inverter = Inverter()
+        display = Display()
 
         while (True):
             inverter.refresh()
-            update_displayed_data(inverter, ote)
+            display.update(inverter, ote)
             time.sleep(10*60)
 
     except IOError as e:
         logging.info(e)
 
     except KeyboardInterrupt:
-        logging.info("ctrl + c:")
-        epd2in13_V2.epdconfig.module_exit(cleanup=True)
         exit()
+
 
 if __name__ == "__main__":
     main()
